1d01ef0a49c0a7628875b66eae2d4a044b729f28
Bojan Vujic <58082605+VujicGit@users.noreply.github.com>
2021-07-09 04:24:40 +0200

    :sparkles: Implemented microservice monitoring

--- src/http/router/router.go
+++ src/http/router/router.go
@@ -2,11 +2,14 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"search-service/http/middleware/prometheus_middleware"
 	"search-service/interactor"
 )
 
 func NewRouter(handler interactor.AppHandler) *gin.Engine {
 	router := gin.Default()
+	requestCoutnter := prometheus_middleware.GetHttpRequestsCounter()
+	router.Use(prometheus_middleware.PrometheusMiddleware(requestCoutnter))
 
 	router.GET("/getLocationsByContaining", handler.GetLocationsByContains)
 	router.GET("/getExactLocation", handler.GetExactLocation)
@@ -15,6 +18,7 @@ func NewRouter(handler interactor.AppHandler) *gin.Engine {
 	router.GET("/getPostsByTag", handler.GetPostsByHashTag)
 	router.POST("/saveNewPostLocation", handler.SaveNewPostLocation)
 	router.POST("/saveNewPostTag", handler.SaveNewPostTag)
+	router.GET("/metrics", prometheus_middleware.PrometheusGinHandler())
 
 	return router
 }
